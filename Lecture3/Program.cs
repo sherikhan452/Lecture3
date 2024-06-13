c1264fdbd49217a99447ebb5a2b474fd29aa1bc3
Admin <sherikhan451@gmail.com>
2024-06-13 20:11:10 +0500

    Add project files.

---
+++ Lecture3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture3
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine("Enter Student Name");
+			String name = Console.ReadLine();
+			Console.WriteLine("Enter Student Roll No");
+			int roll =int.Parse (Console.ReadLine());
+			Console.WriteLine("Enter Maths Marks");
+			int m = int.Parse(Console.ReadLine());
+			Console.WriteLine("Enter Urdu Marks");
+			int u = int.Parse(Console.ReadLine());
+			Console.WriteLine("Enter Physics Marks");
+			int P = int.Parse(Console.ReadLine());
+			Console.WriteLine("Enter Islamiyat Marks");
+			int I = int.Parse(Console.ReadLine());
+			Console.WriteLine("Enter Comp Scie Marks");
+			int cs = int.Parse(Console.ReadLine());
+
+			int total_marks = m + u + P + I + cs;
+			int mar = 500;
+			float marks_obt = total_marks * 100 / 500;
+
+
+			Console.WriteLine("Student Name is: "+name);
+			Console.WriteLine("Student Roll No is: " + roll);
+			Console.WriteLine("Student Maths is: " + m);
+			Console.WriteLine("Student Total Marks is: " + total_marks);
+			Console.WriteLine("Student Percentage is: " + marks_obt+"%");
+
+
+			
+
+
+			Console.ReadLine();
+
+
+
+		}
+	}
+}
