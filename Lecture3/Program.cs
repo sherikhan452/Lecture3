2997d920ac689f81ae85554c12ffb3285c63701f
Admin <sherikhan451@gmail.com>
2024-06-20 20:23:27 +0500

    Lecture 3

--- Lecture3/Program.cs
+++ Lecture3/Program.cs
@@ -11,56 +11,119 @@ namespace Lecture3
 		static void Main(string[] args)
 		{
 
-			Console.WriteLine("Enter Student Name");
-			String name = Console.ReadLine();
-			Console.WriteLine("Enter Student Roll No");
-			int roll =int.Parse (Console.ReadLine());
-			Console.WriteLine("Enter Maths Marks");
-			int m = int.Parse(Console.ReadLine());
-			Console.WriteLine("Enter Urdu Marks");
-			int u = int.Parse(Console.ReadLine());
-			Console.WriteLine("Enter Physics Marks");
-			int P = int.Parse(Console.ReadLine());
-			Console.WriteLine("Enter Islamiyat Marks");
-			int I = int.Parse(Console.ReadLine());
-			Console.WriteLine("Enter Comp Scie Marks");
-			int cs = int.Parse(Console.ReadLine());
 
-			int total_marks = m + u + P + I + cs;
-			//int mar = 500;
-			float marks_obt = total_marks * 100 / 500;
+			/********************* Class 1 ***********************/
 
+			//Console.WriteLine("Enter Student Name");
+			//String name = Console.ReadLine();
+			//Console.WriteLine("Enter Student Roll No");
+			//int roll =int.Parse (Console.ReadLine());
+			//Console.WriteLine("Enter Maths Marks");
+			//int m = int.Parse(Console.ReadLine());
+			//Console.WriteLine("Enter Urdu Marks");
+			//int u = int.Parse(Console.ReadLine());
+			//Console.WriteLine("Enter Physics Marks");
+			//int P = int.Parse(Console.ReadLine());
+			//Console.WriteLine("Enter Islamiyat Marks");
+			//int I = int.Parse(Console.ReadLine());
+			//Console.WriteLine("Enter Comp Scie Marks");
+			//int cs = int.Parse(Console.ReadLine());
 
-			Console.WriteLine("Student Name is: "+name);
-			Console.WriteLine("Student Roll No is: " + roll);
-			Console.WriteLine("Student Maths is: " + m);
-			Console.WriteLine("Student Total Marks is: " + total_marks);
-			Console.WriteLine("Student Percentage is: " + marks_obt+"%");
+			//int total_marks = m + u + P + I + cs;
+			////int mar = 500;
+			//float marks_obt = total_marks * 100 / 500;
 
 
-			if (marks_obt>=80)
+			//Console.WriteLine("Student Name is: "+name);
+			//Console.WriteLine("Student Roll No is: " + roll);
+			//Console.WriteLine("Student Maths is: " + m);
+			//Console.WriteLine("Student Total Marks is: " + total_marks);
+			//Console.WriteLine("Student Percentage is: " + marks_obt+"%");
+
+
+
+			/********************* Class 2 **************************/
+
+			//if (marks_obt>=80)
+			//{
+			//	Console.WriteLine("Hurray Your Grade is: A-1");
+			//}
+			// else if (marks_obt >= 70)
+			//{
+			//	Console.WriteLine("Yup Your Grade is: A");
+			//}
+
+			// else if (marks_obt >= 60)
+			//{
+			//	Console.WriteLine("Yup Your Grade is: B");
+			//}
+
+			// else if (marks_obt >= 50)
+			//{
+			//	Console.WriteLine("Keep it up Your Grade is: C");
+			//}
+
+			//else
+			//{
+			//	Console.WriteLine("Fail");
+			//}
+
+
+
+			/********************* Class 3  **************************/
+
+
+			//int a;
+			//Console.WriteLine("Enter Table Number");
+			//int b=int.Parse(Console.ReadLine());
+
+			//for (a=1;a<=10;a++)
+			//{
+
+
+
+			//	Console.WriteLine(b+ " * " +a+ " = "+b*a);
+
+			//}
+
+
+
+
+			//Jump Statement 
+
+
+			Console.WriteLine("Continue Statement");
+
+			int d;
+
+			for (d=1;d<=10;d++)
 			{
-				Console.WriteLine("Hurray Your Grade is: A-1");
-			}
-			 else if (marks_obt >= 70)
-			{
-				Console.WriteLine("Yup Your Grade is: A");
-			}
+				if (d==7)
+				{
+					continue;
+				}
 
-			 else if (marks_obt >= 60)
-			{
-				Console.WriteLine("Yup Your Grade is: B");
-			}
+				Console.WriteLine(d);
 
-			 else if (marks_obt >= 50)
-			{
-				Console.WriteLine("Keep it up Your Grade is: C");
 			}
+			Console.WriteLine("Loop Terminated");
 
-			else
+			Console.WriteLine("Break Statement");
+
+			int e;
+
+			for (e = 1; e <= 10; e++)
 			{
-				Console.WriteLine("Fail");
+				if (e== 7)
+				{
+					continue;
+				}
+
+				Console.WriteLine(e);
+
 			}
+			Console.WriteLine("Loop Terminated");
+
 
 
 			Console.ReadLine();
